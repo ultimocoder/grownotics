0f7df60c0ba8d60a2609281c024865f157d161e7
ankitsharmawebnotics <130058581+ankitsharmawebnotics@users.noreply.github.com>
2023-04-25 10:28:48 +0530

    last update 24 april

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,17 +2,22 @@ import {createApp} from 'vue'
 import { createStore } from 'vuex'
 import { createRouter,createWebHistory} from 'vue-router'
 import App from './layout/Main.vue'
+import axios from 'axios'
 const AdminLogin = () => import('./components/admin/auth/Login.vue')
 // const AdminRegister = () => import('./components/admin/auth/Register.vue')
 const AdminDashboard = () => import('./components/admin/Dashboard.vue')
+const AdminCategory = () => import('./components/admin/categories/Category.vue')
+const AdminSubcategory = () => import('./components/admin/subcategories/Subcategory.vue')
+const AdminBrand = () => import('./components/admin/brand/Brands.vue')
 
-
+// const getToken = async() => {
+//     await axios.get('/sanctum/csrf-cookie')
+// }
 const router = new createRouter({
     mode: "history",
     history: createWebHistory(),
+    // await: getToken(),
     routes: [
-   
-    
     {
         name: 'AdminLogin',
         path:'/admin/login', 
@@ -27,6 +32,21 @@ const router = new createRouter({
         name: 'AdminDashboard',
         path:'/admin/dashboard', 
         component:AdminDashboard
+    },
+    {
+        name: 'AdminCategory',
+        path:'/admin/category', 
+        component:AdminCategory
+    },
+    {
+        name: 'AdminSubcategory',
+        path:'/admin/subcategory', 
+        component:AdminSubcategory
+    },
+    {
+        name: 'AdminBrand',
+        path:'/admin/brand', 
+        component:AdminBrand
     }
 
 ]
